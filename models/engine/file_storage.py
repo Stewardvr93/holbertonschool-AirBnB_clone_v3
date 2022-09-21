04e00b8ffe8eb0b24d1127ee0299f02ecc5f889e
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-09-21 12:16:31 -0500

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieve an object"""
+        cls_dict = self.all(cls)
+        for obj in cls_dict.values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """Count number of objects in storage"""
+        if cls is None:
+            return len(self.all().values())
+        return len(self.all(cls).values())
