5192fc75116f32cc01e8db5b32e50027d68e7b8c
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-09-26 16:15:40 -0500

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
 """First blueprint for HBNB Project"""
-from flask import Flask, jsonify, make_response
+
+from flask import Flask, render_template, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
-import os
+from models.state import State
+from os import getenv
 from flask_cors import CORS
 
-#Get serious please
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_close(self):
+def cerrar(self):
+    """Delete the current session SQLAlchemy"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def error_handler(error):
+    """No found 404"""
+    return(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST')
-    port = os.getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    host = getenv("HBNB_API_HOST", default="0.0.0.0")
+    port = getenv("HBNB_API_PORT", default="5000")
+    app.run(host=host, port=port, threaded=True,  debug=True)
